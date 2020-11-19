e80484ca3ee585d542496ffb47511812973105a6
vineethm1627 <vineethm1627@gmail.com>
2020-11-19 19:40:44 +0530

    web app

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pixellib
+from pixellib.semantic import semantic_segmentation
+from prediction import *
+
+st.title("Instance Image Segmentation")
+st.sidebar.title("Instance Image Segmentation")
+st.sidebar.markdown("Mask R-CNN model trained on coco dataset for Instance Segmentation using PixelLib. Here, Different instances of the same object are segmented with the different color maps.")
+pic = plt.imread("coco_dataset.png")
+st.sidebar.image(pic, caption = "Model classes", use_column_width = True)
+st.set_option('deprecation.showfileUploaderEncoding', False)
+img_file = st.file_uploader("Upload the input image : ", type = ['jpg', 'jpeg', 'png'])
+
+if img_file is not None:
+    img = plt.imread(img_file, 0)
+    st.image(img, caption = "Input Image", use_column_width = True)
+
+    out, out_box = prediction(img_file)
+    
+    col1, col2 = st.beta_columns(2)
+    col1.image(out, caption = "Segmented Image", use_column_width = True)
+    col2.image(out_box, caption = "Bounding Box", use_column_width = True)
